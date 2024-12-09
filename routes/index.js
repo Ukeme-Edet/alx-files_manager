65001d792ddbe4f7770dc90e84a068ea4cc266c6
Ukeme Edet <ukemeedet2207@gmail.com>
2024-12-09 16:21:44 +0100

    feat: add authentication routes for connect, disconnect, and user profile retrieval

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
+import AuthController from '../controllers/AuthController';
 
 /**
  * Maps the routes to the provided Express application instance.
@@ -10,5 +11,8 @@ const mapRoutes = (app) => {
   app.get('/status', AppController.getStatus);
   app.get('/stats', AppController.getStats);
   app.post('/users', UsersController.postNew);
+  app.get('/connect', AuthController.getConnect);
+  app.get('/disconnect', AuthController.getDisconnect);
+  app.get('/users/me', AuthController.getMe);
 };
 export default mapRoutes;
