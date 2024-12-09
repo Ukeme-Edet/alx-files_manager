28e0854f54220b04e71e5360b62cdf2b4e2d9f74
Ukeme Edet <ukemeedet2207@gmail.com>
2024-12-09 15:13:04 +0100

    feat: enhance DBClient class with connection management and user/file count methods

--- utils/db.js
+++ utils/db.js
@@ -1,7 +1,12 @@
 import { MongoClient } from 'mongodb';
 
+/**
+ * DBClient class to manage database connections and operations.
+ */
 class DBClient {
-  constructor() {
+  /**
+     * Creates an instance of DBClient and connects to the MongoDB database.
+     */ constructor() {
     this.db = null;
     MongoClient.connect(
       `mongodb://${process.env.DB_HOST || 'localhost'}:${
@@ -15,14 +20,26 @@ class DBClient {
     );
   }
 
+  /**
+     * Checks if the database connection is alive.
+     * @returns {boolean} True if the database connection is alive, otherwise false.
+     */
   isAlive() {
     return !!this.db;
   }
 
+  /**
+     * Gets the number of users in the database.
+     * @returns {Promise<number>} The number of users in the database.
+     */
   nbUsers() {
     return this.db.collection('users').countDocuments();
   }
 
+  /**
+     * Gets the number of files in the database.
+     * @returns {Promise<number>} The number of files in the database.
+     */
   nbFiles() {
     return this.db.collection('files').countDocuments();
   }
