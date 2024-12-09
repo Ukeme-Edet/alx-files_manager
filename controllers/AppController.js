4dfb306be18f180ff2b3098ae0b1cd839e5807b4
Ukeme Edet <ukemeedet2207@gmail.com>
2024-12-09 15:32:30 +0100

    feat: add AppController with status and stats endpoints for Redis and DB clients

---
+++ controllers/AppController.js
@@ -0,0 +1,16 @@
+import redisClient from '../utils/redis';
+import dbClient from '../utils/db';
+
+const getStatus = (req, res) => {
+  res.status(200).send({
+    redis: redisClient.isAlive(),
+    db: dbClient.isAlive(),
+  });
+};
+const getStats = async (req, res) => {
+  res.status(200).send({
+    users: await dbClient.nbUsers(),
+    files: await dbClient.nbFiles(),
+  });
+};
+export default { getStatus, getStats };
