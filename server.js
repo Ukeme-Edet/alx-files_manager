10747673b239722a15ef37a3c9c9394eb32b47c2
Ukeme Edet <ukemeedet2207@gmail.com>
2024-12-09 15:32:47 +0100

    feat: initialize Express server and set up routing

---
+++ server.js
@@ -0,0 +1,8 @@
+import express from 'express';
+import mapRoutes from './routes';
+
+const app = express();
+mapRoutes(app);
+app.listen(process.env.PORT || 5000, () => {
+  console.log(`Server is running on port ${process.env.PORT || 5000}`);
+});
