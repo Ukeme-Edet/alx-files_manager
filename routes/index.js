d4404247d6d32339499b632f35fe3e108404fed2
Ukeme Edet <ukemeedet2207@gmail.com>
2024-12-09 15:55:42 +0100

    feat: add route for user registration in index.js

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 import AppController from '../controllers/AppController';
+import UsersController from '../controllers/UsersController';
 
 /**
  * Maps the routes to the provided Express application instance.
@@ -8,5 +9,6 @@ import AppController from '../controllers/AppController';
 const mapRoutes = (app) => {
   app.get('/status', AppController.getStatus);
   app.get('/stats', AppController.getStats);
+  app.post('/users', UsersController.postNew);
 };
 export default mapRoutes;
