fb49175633535795cab271fb51c50e0b4b6b3e5d
Ukeme Edet <ukemeedet2207@gmail.com>
2024-12-09 15:55:49 +0100

    feat: enable JSON parsing for incoming requests in server.js

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ import mapRoutes from './routes';
  * @type {import('express').Express}
  */
 const app = express();
+app.use(express.json());
 mapRoutes(app);
 app.listen(process.env.PORT || 5000, () => {
   console.log(`Server is running on port ${process.env.PORT || 5000}`);
