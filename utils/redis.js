33721736f55f5b31c9700d1477af10c0ae3b3eaf
Ukeme Edet <ukemeedet2207@gmail.com>
2024-12-13 01:17:56 +0100

    feat: enhance RedisClient with connection error handling and status tracking

--- utils/redis.js
+++ utils/redis.js
@@ -9,6 +9,17 @@ class RedisClient {
      */
   constructor() {
     this.client = createClient();
+    this.isClientConnected = true;
+    this.client.on('error', (err) => {
+      console.error(
+        'Redis client failed to connect:',
+        err.message || err.toString(),
+      );
+      this.isClientConnected = false;
+    });
+    this.client.on('connect', () => {
+      this.isClientConnected = true;
+    });
   }
 
   /**
